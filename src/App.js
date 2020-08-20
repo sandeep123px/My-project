6eb460ab55f5d98abb0f86b6d092854e7e60a382
sandeep <sandeepbhaitia9040@gmail.com>
2020-08-20 22:06:44 +0530

    my odmit is here

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+import NavigationBar from "./layout/Navigationbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import CoursesState from "./context/CourseState"
+import AfterResult from "./afterResult";
+
+function App() {
+  return (
+   
+    <CoursesState>
+    <div className="bg">
+      <Router>
+        <Route path="/result" component={AfterResult}/>
+        <NavigationBar />
+        <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+         
+          </Switch>
+         
+        </main>
+       
+      </Router>
+    </div>
+    </CoursesState>
+  );
+}
+
+export default App;
